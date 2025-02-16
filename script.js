d660a90b3ebbc1c16f20f1dfdcfd303f7c258871
keith <147499250+keithcsxyz@users.noreply.github.com>
2025-02-16 23:50:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+////////CONTACT FORMM//////////////
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbz9S1iTmTLJ0LGXvGlprsHXvbk1NtbNbQhQZV0IIyGWq3gnrhbbyKByr6o5p1ex1G4vrA/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById('msg')
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Thanks for reaching out. I'll get back to you soon."
+        setTimeout(function(){
+            msg.innerHTML = ""
+        }, 5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
+
+
+/////////////////////////////////////////
